ad8e53cf27be8ec04fa85d08f798a664bb370f3e
Wade Shuler <wadeshuler@users.noreply.github.com>
2017-04-17 11:52:55 -0400

    add aliases for sub-domain support

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,3 +4,8 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@mainsite', dirname(dirname(__DIR__)) . '/mainsite');
+
+// URL Manager Aliases
+Yii::setAlias('@domainName', (YII_ENV === 'dev') ? 'yii2-members-system.dev' : 'yourlivesite.com');
+Yii::setAlias('@frontendSubdomain', 'users');
+Yii::setAlias('@backendSubdomain', 'admin');
