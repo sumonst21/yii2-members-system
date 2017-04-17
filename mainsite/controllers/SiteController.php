daaf29825892084d755f04cad6c44244f6ed54a1
Wade Shuler <wadeshuler@users.noreply.github.com>
2017-04-17 11:57:22 -0400

    fix AccessControl rules blocking logged in users

--- mainsite/controllers/SiteController.php
+++ mainsite/controllers/SiteController.php
@@ -24,7 +24,6 @@ class SiteController extends BaseController
                 'rules' => [
                     [
                         'allow' => true,
-                        'roles' => ['?'],
                     ],
                 ],
             ],
